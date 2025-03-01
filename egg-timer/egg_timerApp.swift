2aa76a60ab94a64b122a58ca55c34ec9012bcb34
merveogretmek <merve.ogretmek@gmail.com>
2025-03-01 16:47:23 +0100

    Initial Commit

---
+++ egg-timer/egg_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  egg_timerApp.swift
+//  egg-timer
+//
+//  Created by Merve Öğretmek on 1.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct egg_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ egg-timerTests/egg_timerTests.swift
@@ -0,0 +1,17 @@
+//
+//  egg_timerTests.swift
+//  egg-timerTests
+//
+//  Created by Merve Öğretmek on 1.03.2025.
+//
+
+import Testing
+@testable import egg_timer
+
+struct egg_timerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
